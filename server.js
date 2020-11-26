f1ff731ee36cc298ac181c308513ea4307141d4b
IanHinden <Ian.Hinden@gmail.com>
2020-11-25 23:36:16 -0800

    Initial angular page

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser')
+const logger = require('express-logger');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const cors = require('cors');
+const app = express();
+
+const twitter = require('./config/twitter');
 
 const port = 3000;
 
+app.unsubscribe(cors());
+
+app.use(express.static(__dirname + '/public'));  
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 require('./routes/mongoRoutes')(app);
 
