73e79d3682a1d75e070036f10ce5ec75e91c8269
IanHinden <Ian.Hinden@gmail.com>
2020-12-03 00:04:28 -0800

    Grouped jumbotron information together

--- public/js/controllers/idiotController.js
+++ public/js/controllers/idiotController.js
@@ -6,7 +6,7 @@ angular.module('idiotController', [])
         // A call to check if there is currently a session
         $http.get('/api/idiot')
                 .success(function(data) {
-                    let adjusted = data.data[0].followers_count * .4;
+                    let adjusted = Math.floor(data.data[0].followers_count * .4);
                     $scope.idiot = data.data[0].followers_count;
                     $scope.adjusted = adjusted;
                 })
