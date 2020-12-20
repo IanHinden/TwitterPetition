cfd331693bf3238bf419981bc92723b3880a24ad
IanHinden <Ian.Hinden@gmail.com>
2020-12-19 17:33:34 -0800

    Pointed to different databases for different environments

--- config/db.js
+++ config/db.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 
+let db;
+
+if(process.env.NODE_ENV == "local") {
+    db = process.env.DB_DEV_URI;
+} else if (process.env.NODE_ENV == "prod") {
+    db = process.env.DB_PROD_URI;
+}
+
 module.exports = {
-    url : process.env.DB_DEV_URI
+    url : db,
 }
